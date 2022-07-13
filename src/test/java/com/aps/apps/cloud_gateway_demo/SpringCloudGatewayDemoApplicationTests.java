7502c3f29e2fc2cde5a10e30b5ddccbb28bac9b3
Anantha Sharma <Anantha.Sharma4@gmail.com>
2022-07-14 00:17:01 +0530

    initial commit

---
+++ src/test/java/com/aps/apps/cloud_gateway_demo/SpringCloudGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.apps.cloud_gateway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
